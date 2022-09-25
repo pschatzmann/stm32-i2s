ac38a4a0e8c691e19f15fc7db9a1e6ef9c84ed0d
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-09-25 09:30:42 +0200

    hal_conf_extra.h added header

--- src/hal_conf_extra.h
+++ src/hal_conf_extra.h
@@ -1,3 +1,13 @@
+/**
+ * @file hal_conf_extra.h
+ * @author Phil Schatzmann
+ * @version 0.1
+ * @date 2022-09-25
+ * @brief for details see https://github.com/stm32duino/wiki/wiki/HAL-configuration
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #pragma once
 #include <stm32_def.h>
 //#define HAL_ADC_MODULE_DISABLED
